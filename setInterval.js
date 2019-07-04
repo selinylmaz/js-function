be1e8fe870d5a97ff952e2512201e9ef749986f8
selinyilmaz <sselinyilmaz@hotmail.com>
2019-07-04 03:59:40 +0300

    setInterval callback kullanımı

---
+++ setInterval.js
@@ -0,0 +1,4 @@
+//setInterval durdurmadıgımız surece devam eder ekrana 3 sn de bir selam yazmaya setTimeout ise bir defa yazar biter aradaki fark
+setInterval(function(){
+    console.log("selam")
+},3000);
