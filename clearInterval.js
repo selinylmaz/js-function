331a6bc444050e3cf771a8739a82ee97b7604940
selinyilmaz <sselinyilmaz@hotmail.com>
2019-07-04 04:10:04 +0300

    setInterval + clearInterval kullanımı

---
+++ clearInterval.js
@@ -0,0 +1,10 @@
+//clearInterval durdurur,sayi 5 e esit oldugunda program dursun
+ 
+var sayi=1;
+
+var interval=setInterval(function(){
+    if(sayi==5)
+    clearInterval(interval);
+    console.log("selam:"+sayi);
+    sayi++;
+},1000);
