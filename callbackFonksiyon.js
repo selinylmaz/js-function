0dba196332372498213ae24656ea5a0f02cc3d77
selinyilmaz <sselinyilmaz@hotmail.com>
2019-07-04 04:21:06 +0300

    callback kullanımı baslangıcı

---
+++ callbackFonksiyon.js
@@ -0,0 +1,12 @@
+function birinci(){
+    console.log("birinci");
+}
+function ikinci(){
+    console.log("ikinci");
+}
+function ucuncu(){
+    console.log("ucuncu");
+}
+birinci();
+ikinci();
+ucuncu();
