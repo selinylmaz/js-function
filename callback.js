8da3392353cdff584a1fb90eb1105fff132db0c8
selinyilmaz <sselinyilmaz@hotmail.com>
2019-07-04 03:50:02 +0300

    degisken olarak callback kullanımı

--- callback.js
+++ callback.js
@@ -6,3 +6,12 @@ function cayDemle(){
     console.log("cay demleniyor..");
 }
 soyle("merhaba",cayDemle);
+//degişkene fonksiyon atamayla callback yaptım
+function soyle1(text,callback){
+    console.log(text);
+    callback();
+}
+var cayDemle1=function (){
+    console.log("cay demleniyor");
+}
+soyle1("merhaba",cayDemle1);
